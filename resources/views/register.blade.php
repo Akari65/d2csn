4b40c437690a295003ee5d2891bc19d08b03f060
Akari65 <nimdrew365@gmail.com>
2023-06-13 14:15:50 +0530

    first commit

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Login Form</title>
+  <title>Registration Form</title>
+  @include('common.css')
   <style>
     body {
       font-family: Arial, sans-serif;
@@ -49,16 +50,64 @@
   </style>
 </head>
 <body>
-  <div class="container">
-    <h2>Register Form</h2>
-    <p>{{$registration_txt}}</p>
-    <form action="/login" method="post">
-      <label for="username">Username:</label>
-      <input type="text" id="username" name="username" required>
-      <label for="password">Password:</label>
-      <input type="password" id="password" name="password" required>
-      <button type="submit">Register</button>
-    </form>
-  </div>
+  <!-- contact section -->
+
+  <section class="contact_section layout_padding">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-5 col-lg-4 offset-md-1 offset-lg-2">
+          <div class="form_container">
+            <div class="heading_container">
+              <h2>
+                Registration form
+               
+            </div>
+            
+            <form action="/register_new_user" method="POST">
+            @csrf
+              <div>
+                <label for="full name">Full name:</label>
+                <input type="text" placeholder="Full Name " name="full_name" id="full_name" />
+              </div>
+              <div>
+              <label for="email">Email:</label>
+                <input type="email" placeholder="Email" id="email" name="email"/>
+              </div>
+              <div>
+              <label for="phone_number">Phone number:</label>
+                <input type="text" placeholder="Phone_number" id="Phone_number"/>
+              </div>
+              <div>
+              <label for="password">Password:</label>
+                <input type="text" placeholder="Password" id="password" name="password"required />
+              </div>
+             <div className="dropdown">
+              <label for="role">Role:</label>
+              <select name="role" id="role">
+                <option value="">Select role</option>
+                @foreach ($response_data as $data)
+                <option value="{{ $data->id }}">{{ $data->name}} </option>
+                @endforeach
+               </select>
+              <div class="d-flex ">
+                <button>
+                  SEND
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div class="col-md-6  px-0">
+          <div class="map_container">
+            <div class="map">
+              <div id="googleMap"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- end contact section -->
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -21,15 +21,16 @@ Route::get('login', function () {
     return view('login');
 });
 
-Route::get('register', [App\Http\Controllers\RegistrationController::class,'Register']);
+Route::get('register', [App\Http\Controllers\registration\RegistrationController::class,'Register']);
 Route::get('create_role_index', function () {
     return view('create_role_index');
 });
 
-Route::get('get_role_list/{param_type}/{id}/{status}', [App\Http\Controllers\RegistrationController::class,'get_role_list']);
+Route::get('get_role_list/{param_type}/{id}/{status}', [App\Http\Controllers\registration\RegistrationController::class,'get_role_list']);
 
-Route::post('create_role', [App\Http\Controllers\RegistrationController::class,'create_role']);
-Route::post('update_role', [App\Http\Controllers\RegistrationController::class,'update_role']);
-Route::get('edit_role/{id}', [App\Http\Controllers\RegistrationController::class,'edit_role']);
-Route::get('delete_role/{id}', [App\Http\Controllers\RegistrationController::class,'delete_role']);
+Route::post('create_role', [App\Http\Controllers\registration\RegistrationController::class,'create_role']);
+Route::post('update_role', [App\Http\Controllers\registration\RegistrationController::class,'update_role']);
+Route::get('edit_role/{id}', [App\Http\Controllers\registration\RegistrationController::class,'edit_role']);
+Route::get('delete_role/{id}', [App\Http\Controllers\registration\RegistrationController::class,'delete_role']);
 
+Route::post('register_new_user', [App\Http\Controllers\registration\RegistrationController::class,'register_new_user']);
